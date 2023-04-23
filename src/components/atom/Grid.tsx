90d465c9a1082783c7e30bc9755cdb296efb9d72
howdyfrom2019 <tjd1emd@naver.com>
2023-04-23 13:29:56 +0900

    feat: add msg by current hour

--- src/components/atom/Grid.tsx
+++ src/components/atom/Grid.tsx
@@ -1,26 +1,11 @@
-'use client'
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import GridImg from '@/assets/grid.png';
 
 const Grid = ({ children }: { children?: React.ReactNode }) => {
-  const [gridW, setGridW] = useState(0);
-
-  const onHandleGridW = () => {
-    setGridW(window.innerWidth - 48);
-  }
-
-  useEffect(() => {
-    if (!window) return;
-    onHandleGridW();
-
-    window.addEventListener('resize', onHandleGridW, false);
-
-    return () => { window.removeEventListener('resize', onHandleGridW, false) }
-  }, []);
 
   return (
     <div className={'relative flex flex-col flex-wrap w-full mt-px'}>
-      <Image src={'/img/grid.png'} alt={'grid'} width={gridW} height={Math.floor(241 * 1234 / gridW)} />
+      <Image className={'w-full'} src={GridImg} alt={'grid'} />
       {children}
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,8 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
