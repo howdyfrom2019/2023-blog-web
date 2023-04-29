f586f95d3a4080639a23caafd4c260d86d283b34
jamie-jungah <wjddk718@gmail.com>
2023-04-29 14:08:18 +0900

    feat: add footer component

--- src/components/atom/Logo.tsx
+++ src/components/atom/Logo.tsx
@@ -1,11 +1,11 @@
 'use client'
 import Jake from '@/assets/logo.svg';
 
-const Logo = () => {
+const Logo = ({ className }: { className?: string }) => {
   return (
-    <figure className={'flex gap-2 items-center'}>
+    <figure className={`flex gap-2 items-center ${className}`}>
       <Jake width={'24'} height={'24'} viewBox={'0 0 128 128'} />
-      <figcaption className={'font-bold translate-y-px'}>Jake.log</figcaption>
+      <figcaption className={'font-bold-16 translate-y-px'}>Jake.log</figcaption>
     </figure>
   )
 }
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -10,7 +10,6 @@ export interface TimeStateType {
 export function getTimeState(): TimeStateType {
   const date = new Date();
   const hour = date.getHours();
-  console.log(hour);
 
   if (hour >= 6 && hour < 12) {
     return { state: 'GM', msg: '좋은 아침입니다. 아침에는 커피 한 잔으로 시작하는 게 어떨까요?' };
